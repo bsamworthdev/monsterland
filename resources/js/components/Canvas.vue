650141af019aa530f83f966b59abfad0455d4aa2
bsamworthdev <bsamworthdev@outlook.com>
2020-06-23 21:49:23 +0000

    Added canvas and colours

--- resources/js/components/Canvas.vue
+++ resources/js/components/Canvas.vue
@@ -1,14 +1,26 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
                         <h3>Canvas</h3>
+                        <ul class="demoToolList">
+                            <li>Clear the canvas: <button id="clearCanvasSimpleColors" @click="clearCanvas" type="button">Clear</button></li>
+                            <li>
+                                <span class="highlight">Choose a colour: </span>
+                                <button id="chooseBlackSimpleColors" @click="chooseColor('black')" type="button">Black</button>
+                                <button id="choosePurpleSimpleColors" @click="chooseColor('purple')" type="button">Purple</button>
+                                <button id="chooseGreenSimpleColors" @click="chooseColor('green')" type="button">Green</button>
+                                <button id="chooseYellowSimpleColors" @click="chooseColor('yellow')" type="button">Yellow</button>
+                                <button id="chooseBrownSimpleColors" @click="chooseColor('brown')" type="button">Brown</button>
+                            </li>
+                        </ul>
                         <div id="canvasDiv" 
                             @mousedown="mouseDown($event)" 
+                            @mouseup="mouseUp($event)" 
                             @mousemove="mouseMove($event)" 
                             @mouseleave="mouseLeave($event)">
                         </div>
@@ -23,46 +35,64 @@
     export default {
         methods: {
             mouseDown: function(e){
-                var mouseX = e.pageX - this.offsetLeft;
-                var mouseY = e.pageY - this.offsetTop;
+                var mouseX = e.offsetX;
+                var mouseY = e.offsetY;
                         
                 this.paint = true;
-                addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-                redraw();
+                this.addClick(mouseX, mouseY);
+                this.redraw();
             },
-            mousemove: function(e){
+            mouseUp: function(e){
+                this.paint = false;
+            },
+            mouseMove: function(e){
                 if(this.paint){
-                    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-                    redraw();
+                    var mouseX = e.offsetX;
+                    var mouseY = e.offsetY;
+                    this.addClick(mouseX, mouseY, true);
+                    this.redraw();
                 }
             },
-            mouseleave: function(e){
+            mouseLeave: function(e){
                 this.paint = false;
             },
             addClick: function(x, y, dragging) {
                 this.clickX.push(x);
                 this.clickY.push(y);
                 this.clickDrag.push(dragging);
+                this.clickColor.push(this.curColor);
             },
             redraw: function() {
+                var clickX = this.clickX;
+                var clickY = this.clickY;
+                var clickDrag = this.clickDrag;
                 var context = this.context;
-                context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+                this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height); // Clears the canvas
                 
-                context.strokeStyle = "#df4b26";
-                context.lineJoin = "round";
-                context.lineWidth = 5;
+                // this.context.strokeStyle = "#df4b26";
+                this.context.lineJoin = "round";
+                this.context.lineWidth = 5;
                             
                 for(var i=0; i < clickX.length; i++) {		
-                    context.beginPath();
+                    this.context.beginPath();
                     if (clickDrag[i] && i){
-                        context.moveTo(clickX[i-1], clickY[i-1]);
+                        this.context.moveTo(clickX[i-1], clickY[i-1]);
                     } else{
-                        context.moveTo(clickX[i]-1, clickY[i]);
+                        this.context.moveTo(clickX[i]-1, clickY[i]);
                     }
-                    context.lineTo(clickX[i], clickY[i]);
-                    context.closePath();
-                    context.stroke();
+                    this.context.lineTo(clickX[i], clickY[i]);
+                    this.context.closePath();
+                    this.context.strokeStyle = this.color[this.clickColor[i]];
+                    this.context.stroke();
                 }
+            },
+            clearCanvas: function(){
+                this.context.fillStyle = '#ffffff'; // Work around for Chrome
+                this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight); // Fill in the canvas with white
+                this.context.width = this.context.width; // clears the canvas 
+            },
+            chooseColor: function(colorName) {
+                this.curColor = colorName;
             }
         },
         data() {
@@ -71,14 +101,26 @@
                 clickX: [],
                 clickY: [],
                 clickDrag: [],
-                paint: ''
+                paint: '',
+                canvasWidth: 616,
+                canvasHeight: 300,
+                color:{
+                    "black" : "#000000",
+                    "purple" : "#cb3594",
+                    "green" : "#659b41",
+                    "yellow" : "#ffcf33",
+                    "brown" : "#986928"
+                },
+                curColor: 'black',
+                clickColor:[]
+
             }
         },
         mounted() {
             var canvasDiv = document.getElementById('canvasDiv');
             var canvas = document.createElement('canvas');
-            canvas.setAttribute('width', 500); //canvasWidth
-            canvas.setAttribute('height', 300); //canvasHeight
+            canvas.setAttribute('width', this.canvasWidth);
+            canvas.setAttribute('height', this.canvasHeight);
             canvas.setAttribute('id', 'canvas');
             canvasDiv.appendChild(canvas);
             if(typeof G_vmlCanvasManager != 'undefined') {
@@ -89,3 +131,11 @@
         }
     }
 </script>
+
+<style scoped>
+#canvasDiv{
+    border: 1px solid black;
+    width:616px;
+    height:300px;
+}
+</style>
