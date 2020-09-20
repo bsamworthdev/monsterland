930d5ae96d7947f5df5c48360cf2acb042bfb267
Ben Samworth <bsamworthdev@outlook.co.uk>
2020-09-20 22:57:34 +0100

    Show image in shared link

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
+@section('head')
+<script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=5f67b48601b108001af03582&product=image-share-buttons" async="async"></script>
+<meta property="og:title" content="Look at this monster I created on monsterland.net" />
+<meta property="og:url" content="https://monsterland.net" />
+<meta property="og:description" content="Monsterland is an online drawing game, where you collaborate with other 
+artists to create wonderful or hideous monsters. It's free and has no ads. You don't even need an account. 
+Why not come and have a go!" />
+<meta property="og:site_name" content="Monsterland" />
+<meta property="og:image" content="https://monsterland.net/storage/{{ $monster->id }}.jpg" />
+@stop
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
