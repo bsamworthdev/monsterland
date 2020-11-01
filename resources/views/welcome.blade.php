2740673aca7cf350f64f5877489ddef6a3e5c5bb
Ben Samworth <bsamworthdev@outlook.co.uk>
2020-11-01 20:42:48 +0000

    Change back to normal background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,8 +27,8 @@
             }
 
             body{
-                /*background-image:url('{{ asset('images/countryside_bg.jpg') }}'); */
-                background-image:url('{{ asset('images/halloween_bg.jpg') }}');
+                background-image:url('{{ asset('images/countryside_bg.jpg') }}');
+                /*background-image:url('{{ asset('images/halloween_bg.jpg') }}'); */
                 background-repeat: no-repeat;
                 background-size: cover;
                 background-attachment: fixed;
