5e5a6af2e4176b5cf867e4d854abd4b2a9d411bb
Ben Samworth <bsamworthdev@outlook.co.uk>
2020-12-04 16:17:11 +0000

    Increase login session to 1 week

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
                                                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                                             <label class="form-check-label" for="remember">
-                                                                {{ __('Remember Me') }}
+                                                                Stay signed in for a week
                                                             </label>
                                                         </div>
                                                     </div>
