5b0dca90bb749a20b91d4ebe129bedc21003025d
Ben Samworth <bsamworthdev@outlook.co.uk>
2021-01-25 17:16:35 +0000

    Added dot to check versioning

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
                                                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                                             <label class="form-check-label" for="remember">
-                                                                Stay signed in for a week
+                                                                Stay signed in for a week.
                                                             </label>
                                                         </div>
                                                     </div>
