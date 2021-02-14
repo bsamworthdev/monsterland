26e1cf9c0a08c3083a8d5d6445df1aed0f0cf002
Ben Samworth <bsamworthdev@outlook.co.uk>
2021-02-14 09:35:38 +0000

    Added Valentines background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,8 @@
                 z-index: -1;   
             } */
             body {
-                background-image:url('{{ asset('images/countryside_bg.jpg') }}');
+                background-image:url('{{ asset('images/valentines_bg.jpg') }}');
+                /*background-image:url('{{ asset('images/countryside_bg.jpg') }}');*/
                 /*background-image:url('{{ asset('images/halloween_bg.jpg') }}'); */
                 /* background-image:url('{{ asset('images/christmas_bg3.jpg') }}');  */
                 background-size: cover;
