590e03c2a445f0085df2c2ba067da303ba48ed20
Ben Samworth <bsamworthdev@outlook.co.uk>
2021-04-15 11:23:11 +0100

    Added tagging leaderboard stats

---
+++ resources/js/components/OverallStats.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+        <div class="container">
+            <div class="row">
+                <label>Monsters Created: {{ overallStats.monster_count }}</label>
+            </div>
+            <div class="row">
+                <label>Monsters With Tags:</label>
+                <div class="progress w-100">
+                    <div class="progress-bar" role="progressbar" :style="'width: ' + overallStats.tagged_percent + '%;'" :aria-valuenow="overallStats.tagged_percent" aria-valuemin="0" aria-valuemax="100">{{ overallStats.tagged_percent }}%</div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+          overallStats: Object
+        },
+        methods: {
+            
+        },
+        computed: {
+           
+        },
+        mounted() {
+            console.log('Component mounted.')
+            
+           
+        },
+    }
+</script>
+
+<style scoped>
+
+
+</style>
